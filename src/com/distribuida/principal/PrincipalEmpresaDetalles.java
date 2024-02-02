da572ed4d1bee1420ef6c84ca0833c7c4b6b36cb
miguelquipo <quipomiguel42@gmail.com>
2024-02-02 12:09:56 -0500

    v3.0.17 DAO Empresas

--- src/com/distribuida/principal/PrincipalEmpresaDetalles.java
+++ src/com/distribuida/principal/PrincipalEmpresaDetalles.java
@@ -1,10 +1,20 @@
 package com.distribuida.principal;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.distribuida.dao.Empresa_detallesDAO;
+
 public class PrincipalEmpresaDetalles {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		Empresa_detallesDAO enpresaDetalleDAO = context.getBean("empresa_detallesDAOImpl",Empresa_detallesDAO.class);
+		
+		
+		
+		context.close();
+		
 	}
 
 }
